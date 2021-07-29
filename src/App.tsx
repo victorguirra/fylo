263c69beb328caa12c9d78163a8a635e0a1c3759
victorguirra <victor.hugo.guirra@gmail.com>
2021-07-28 21:42:05 -0300

    config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Routes } from './routes';
+
 export function App(){
   return(
-    <h1>Hello World</h1>
+    <Routes />
   )
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+export function Home(){
+    return(
+        <Container>
+            <h1>Hello Home Page</h1>
+        </Container>
+    )
+}
