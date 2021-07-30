a92c1bde106f3cbb2e9faf1fadccf7f89672141c
victorguirra <victor.hugo.guirra@gmail.com>
2021-07-30 16:19:57 -0300

    responsive features in main section

--- src/pages/Home/sections/Main/Features/styles.ts
+++ src/pages/Home/sections/Main/Features/styles.ts
@@ -12,4 +12,14 @@ export const Container = styled.section`
         width:48%;
         margin-bottom:8rem;
     }
+
+    @media(max-width:1240px){
+        width:95%;
+        flex-direction:column;
+
+        .featureCard {
+            width:100%;
+            margin-bottom:5rem;
+        }
+    }
 `;
