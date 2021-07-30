72d8cb4d5c2d82f4f37d977944df3ce69c56a450
victorguirra <victor.hugo.guirra@gmail.com>
2021-07-29 22:46:26 -0300

    finished footer

--- src/pages/Home/sections/Footer/index.tsx
+++ src/pages/Home/sections/Footer/index.tsx
@@ -1,9 +1,136 @@
-import { Container } from './styles';
+import {
+    Container,
+    Logo,
+    Wrapper,
+    Location,
+    Contacts,
+    Column,
+    Social
+} from './styles';
+
+import { useTheme } from 'styled-components';
+
+import logoImg from '../../../../images/logo.svg';
+import locationImg from '../../../../images/icon-location.svg';
+import phoneImg from '../../../../images/icon-phone.svg';
+import emailImg from '../../../../images/icon-email.svg';
+
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+interface IContactsData {
+    title: string;
+    icon: string;
+    text: string;
+}
+
+interface IColumnItemsData {
+    title:string;
+    link:string;
+}
+
+interface ISocialMediasData {
+    title: string;
+    icon: JSX.Element;
+}
 
 export function Footer(){
+    const { colors } = useTheme();
+
+    const contacts: IContactsData[] = [
+        {
+            title:'Phone',
+            icon: phoneImg,
+            text: '+1-543-123-4567'
+        },
+        {
+            title:'Email',
+            icon: emailImg,
+            text: 'example@fylo.com'
+        },
+    ];
+
+    const firstColumnItemsData: IColumnItemsData[] = [
+        { title:'About Us', link: '#' },
+        { title:'Jobs', link: '#' },
+        { title:'Press', link: '#' },
+        { title:'Blog', link: '#' },
+    ]; 
+
+    const secondColumnItemsData: IColumnItemsData[] = [
+        { title:'Contact Us', link: '#' },
+        { title:'Terms', link: '#' },
+        { title:'Privacy', link: '#' },
+    ];
+
+    const socialMediasData: ISocialMediasData[] = [
+        { 
+            title: 'Facebook', 
+            icon: <FaFacebookF size={ 15 } color={ colors.text_primary } /> 
+        },
+        { 
+            title: 'Twitter', 
+            icon: <FaTwitter size={ 15 } color={ colors.text_primary } /> 
+        },
+        { 
+            title: 'Instagram', 
+            icon: <FaInstagram size={ 15 } color={ colors.text_primary } /> 
+        },
+    ];
+
     return(
         <Container>
-            <h1>Hello Footer</h1>
+            <Logo src={ logoImg } alt="Fylo" />
+
+            <Wrapper>
+                <Location>
+                    <img src={ locationImg } alt="Location" />
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur <br/>
+                        adispiscing elit, sed do eiusmod tempor <br/>
+                        incididunt ut labore et dolore magna aliqua
+                    </p>
+                </Location>
+
+                <Contacts>
+                    { contacts.map((item: IContactsData, index) => (
+                        <div key={ index }>
+                            <img src={ item.icon } alt={ item.title } />
+                            <span>{ item.text }</span>
+                        </div>
+                    ))}
+                </Contacts>
+
+                <Column>
+                    { firstColumnItemsData.map((item: IColumnItemsData, index) => (
+                        <a 
+                            key={ index }
+                            href={ item.link }
+                        >
+                            { item.title }
+                        </a>
+                    ))}
+                </Column>
+
+                <Column>
+                    { secondColumnItemsData.map((item: IColumnItemsData, index) => (
+                        <a
+                            key={ index }
+                            href={ item.link }
+                        >
+                            { item.title }
+                        </a>
+                    ))}
+                </Column>
+
+                <Social>
+                    { socialMediasData.map((item: ISocialMediasData, index) => (
+                        <div key={ index }>
+                            { item.icon }
+                        </div>
+                    ))}
+                </Social>
+            </Wrapper>
         </Container>
     )
 }
--- src/pages/Home/sections/Footer/styles.ts
+++ src/pages/Home/sections/Footer/styles.ts
@@ -2,8 +2,93 @@ import styled from 'styled-components';
 
 export const Container = styled.footer`
     background-color: ${({ theme }) => theme.colors.background_footer };
+
+    padding:0 8rem 3rem;
+`;
+
+export const Logo = styled.img`
+    width:10rem;
+`;
+
+export const Wrapper = styled.div`
+    width:100%;
+    margin-top:3rem;
+
+    display:flex;
+    justify-content: space-between;
+`;
+
+export const Location = styled.div`
+    display:flex;
     
-    h1 {
-        color:#fff;
+    img {
+        width:.8rem;
+        height:1rem;
+    }
+
+    p {
+        color:${({ theme }) => theme.colors.text_primary };
+        font-family:${({ theme }) => theme.fonts.primary };
+        font-size:.9rem;
+        line-height:1.4rem;
+
+        margin-left:1.6rem;
+    }
+`;
+
+export const Contacts = styled.div`
+    display:flex;
+    flex-direction:column;
+    
+    > div {
+        display:flex;
+        align-items:center;
+
+        &:not(:first-of-type) {
+            margin-top:1.2rem;
+        }
+
+        span {
+            color:${({ theme }) => theme.colors.text_primary };
+            font-family:${({ theme }) => theme.fonts.primary };
+
+            margin-left:1.2rem;
+        }
+    }
+`;
+
+export const Column = styled.div`
+    display:flex;
+    flex-direction:column;
+    
+    a {
+        color:${({ theme }) => theme.colors.text_primary };
+        font-family:${({ theme }) => theme.fonts.primary };
+        
+        text-decoration:none;
+
+        &:not(:first-of-type) {
+            margin-top:1.2rem;
+        }
+    }
+`;
+
+export const Social = styled.div`
+    display:flex;
+    
+    div {
+        width:2.4rem;
+        height:2.4rem;
+
+        border:.12rem solid ${({ theme }) => theme.colors.text_primary };
+        border-radius:50%;
+
+        display:flex;
+        align-items:center;
+        justify-content:center;
+
+        &:not(:last-of-type) {
+            margin-right:1rem;
+        }
     }
 `;
